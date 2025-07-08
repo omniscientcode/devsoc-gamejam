fd9bf465e7c346d0ff0952ebcd930477f55c14fc
Edward Zhang <z5589190@ad.unsw.edu.au>
2025-07-08 14:09:15 +1000

    edward

--- entities/entity.gd
+++ entities/entity.gd
@@ -1,17 +1,22 @@
-extends Node2D
+extends CharacterBody2D
 
 class_name entity
 
 @export var HP = 100.0; 
 @export var SPEED = 100.0
+@export var ACCELERATION = 100
+
 @export var ATTACK = 20.0
 @export var DAMAGE_RESISTANCE = 0.0
 @export var CRIT_CHANCE = 0.05
 @export var ATTACK_SPEED = 1.0
 
+const FRICTION = 100
+
 @onready var attack_range: Area2D = %attackRange
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 @onready var attack_timer: Timer = %AttackTimer
+@onready var collision_detection: CollisionShape2D = $CollisionDetection
 
 var enemy_array = [];
 
@@ -26,12 +31,12 @@ func _connect_processes() -> void:
 	if not attack_range.is_connected("area_exited", Callable(self, "_on_attack_range_area_exited")):
 		attack_range.connect("area_exited", Callable(self, "_on_attack_range_area_exited"))
 
-func _physics_process(delta: float) -> void: 
+func _physics_process(_delta: float) -> void: 
 	attackCheck()
 
 # Checks if can attack
 func attackCheck():
-	if (enemy_array.length() > 0 && attack_timer.is_stopped()):
+	if (enemy_array.size() > 0 && attack_timer.is_stopped()):
 		animated_sprite_2d.play("attack")
 		attackEnemies()
 		attack_timer.start()
@@ -43,9 +48,9 @@ func attackEnemies():
 	for enemy in enemy_array:
 		enemy.damage(ATTACK)
 
-func damage(ATTACK):
-	var damageTaken = max(ATTACK * (1 - DAMAGE_RESISTANCE), 0.0)
-	HP -= damageTaken
+func damage(damageTaken):
+	var actualDamageTaken = max(damageTaken * (1 - DAMAGE_RESISTANCE), 0.0)
+	HP -= actualDamageTaken
 	if HP <= 0:
 		death()
 
@@ -59,9 +64,9 @@ func _on_attack_timer_timeout() -> void:
 	attack_timer.start()
 
 # If enemy enters, add to attack array
-func _on_attack_range_area_entered(area: Area2D) -> void:
+func _on_attack_range_area_entered(_area: Area2D) -> void:
 	pass
 
 # If enemy dies, remove from array
-func _on_attack_range_area_exited(area: Area2D) -> void:
+func _on_attack_range_area_exited(_area: Area2D) -> void:
 	pass
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,16 +1,21 @@
 extends entity
 
+var moveVector = Vector2.RIGHT
+
 func _physics_process(delta: float) -> void:
 	# Update character stats 
 	updateStats()
 	attackCheck()
-	if enemy_array.length() == 0:
+	if enemy_array.size() == 0:
 		attack_timer.stop()
 		moveCharacter()
 
 # move character function
 func	 moveCharacter():
 	animated_sprite_2d.play("move")
+	velocity =  velocity.move_toward(Vector2.RIGHT * SPEED, ACCELERATION)
+	move_and_slide()
+	
 
 # Function that updates player stats
 func updateStats():
