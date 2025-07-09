8c1e73fd23bdcda95615aaca65f1e5e14d9f7c59
Ricky Guan <z5591153@ad.unsw.edu.au>
2025-07-09 12:26:43 +1000

    finished timer

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @onready var gacha_button: Button = $GachaButton/GachaButtonPress
 @onready var unsw_background_png: Sprite2D = $"Unsw-background_png"
+@onready var timer_button: Button = $TimerButton/TimerButtonPress
+@onready var pomodoro_timer: Control = $PomodoroTimer
+@onready var skills_tab: Control = $SkillsTab
+@onready var skills_button: Button = $SkillsButton/SkillsButtonPress
 
 const BOSS_BG = preload("res://assets/images/bg/boss_battle.png")
 const DEFAULT_BG = preload("res://assets/images/bg/unsw-background.png.png")
@@ -10,6 +14,8 @@ const DEFAULT_BG = preload("res://assets/images/bg/unsw-background.png.png")
 func _ready() -> void:
 	GlobalVariables.connect("boss_changed", Callable(self, "_bg_change"))
 	gacha_button.pressed.connect(Callable(self, "_on_gacha_button_pressed"))
+	timer_button.pressed.connect(Callable(self, "_on_timer_button_pressed"))
+	skills_button.pressed.connect(Callable(self, "_on_skill_button_pressed"))
 	
 func _on_gacha_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/gacha.tscn")
@@ -20,3 +26,11 @@ func _bg_change() -> void:
 	if GlobalVariables.boss_alive == false:
 		await get_tree().create_timer(0.8).timeout
 		unsw_background_png.texture = DEFAULT_BG
+		
+func _on_timer_button_pressed() -> void:
+	skills_tab.visible = false
+	pomodoro_timer.visible = true
+	
+func _on_skill_button_pressed() -> void:
+	skills_tab.visible = true
+	pomodoro_timer.visible = false
